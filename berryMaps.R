30160b690ddc2e8db1d64881c88d32b21a65e3d7
brry <berryboessenkool@hotmail.com>
2024-04-05 16:58:46 +0200

    mixdorf

--- berryMaps.R
+++ berryMaps.R
@@ -13,8 +13,8 @@ library(osmdata) # opq, add_osm_feature, osmdata_sf
 
 loc <- read.table(header=TRUE, sep=",", text="
 n, y     ,  x     , zm, t    , l    , b    , r    ,sel,Ort
-1, 53.248,  12.652, 15, 53.29, 12.58, 53.21, 12.73,T,Sewekow
-2, 52.375,  13.125, 14, 0    ,     0,     0,     0,T,Potsdam
+1, 53.248,  12.652, 15, 53.29, 12.58, 53.21, 12.73,F,Sewekow
+2, 52.375,  13.125, 14, 0    ,     0,     0,     0,F,Potsdam
 3, 52.545,  14.08 , 15, 52.56, 14.02, 52.52, 14.12,F,Waldsieversdorf
 4, 53.21 ,  13.32 , 13, 53.24, 13.24, 53.17, 13.42,F,Lychen
 5, 48.67 ,  10.70 , 15, 48.69, 10.65, 48.63, 10.76,F,Tapfheim
@@ -31,6 +31,7 @@ n, y     ,  x     , zm, t    , l    , b    , r    ,sel,Ort
 16,28.967, -13.670, 15, 0    ,     0,     0,     0,F,Lanzarote
 17,56.372,  15.517, 15, 0    ,     0,     0,     0,F,Stensjoe
 18,54.440,  12.683, 15, 0.   ,     0,     0,     0,F,Zingst
+19,52.204,  14.430, 15, 0.   ,     0,     0,     0,T,Mixdorf
 ")
 loc$t[loc$t==0] <- loc$y[loc$t==0]+0.05
 loc$b[loc$b==0] <- loc$y[loc$b==0]-0.05 # loc$b <- ifelse(loc$b==0, loc$y-0.05, loc$b)
@@ -39,7 +40,7 @@ loc$l[loc$l==0] <- loc$x[loc$l==0]-0.08
 loc$r[loc$r==0] <- loc$x[loc$r==0]+0.08
 
 
-startview <- 2
+startview <- 19
 if(F){
 bnd <- loc[startview,c("l","t","r","b")]
 leaflet() %>% addTiles() %>% 
